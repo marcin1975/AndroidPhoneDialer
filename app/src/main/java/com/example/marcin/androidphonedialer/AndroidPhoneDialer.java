354eeaf24e78de9b35bbcc82efccbabb0bf8c1b1
Marcin1975 <marcin.kopaczynski@gmx.at>
2015-07-11 01:02:34 +0200

    With this app a call can be made.

--- app/src/main/java/com/example/marcin/androidphonedialer/AndroidPhoneDialer.java
+++ app/src/main/java/com/example/marcin/androidphonedialer/AndroidPhoneDialer.java
@@ -1,38 +1,25 @@
 package com.example.marcin.androidphonedialer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class AndroidPhoneDialer extends ActionBarActivity {
-
+public class AndroidPhoneDialer extends Activity {
+    /** Called when activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_android_phone_dialer);
+        setContentView(R.layout.main_activity_android_phone_dialer);
+        /** Create Intent to call the Dialer */
+        /** Pass the Dialer the number +4366488915200 */
+        Intent DialIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+4366488915200"));
+        /** Use NEW_TASK_LAUNCH to launch the Dialer Activity */
+        DialIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        /** Finally start the Activity */
+        startActivity(DialIntent);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_android_phone_dialer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
